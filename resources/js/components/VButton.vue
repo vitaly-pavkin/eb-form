99629c94f510e9eb62a9520cc488a7f127017d65
Vitaliy Pavkin <pavkinv20@gmail.com>
2020-11-24 20:09:26 +0700

    feat: implement form

---
+++ resources/js/components/VButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <button
+        type="submit"
+        class="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none"
+    >
+        <slot />
+    </button>
+</template>
+
+<script>
+    export default {
+        name: "Button"
+    }
+</script>
---
+++ resources/js/components/VTextArea.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "VTextArea"
+    }
+</script>
+
+<style scoped>
+
+</style>
